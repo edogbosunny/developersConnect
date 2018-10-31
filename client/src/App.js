d4caadb6685737e1ae10c973c5e4a648ba266c84
edogbosunny <edogbosunday@gmail.com>
2018-10-31 17:22:21 +0100

    immplement post comment functionality): - implement react for the front end

---
+++ client/src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
+import Navbar from './component/layout/Navbar';
+import Footer from './component/layout/Footer';
+import Landing from './component/layout/Landing';
+import Register from './component/layout/auth/Register';
+import Login from './component/layout/auth/Login';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+      <div className='App'>
+        <Navbar />
+        <Route exact path ='/' component={ Landing } />
+        <div className = 'container'>
+        <Route exact path='/register' component = { Register }/>
+        <Route exact path='/login' component = { Login }/>
+          </div>
+        <Footer />
+      </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
